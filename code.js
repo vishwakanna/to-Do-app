3ed88b071a56e5e7214e5923efdfa7d1b5fc8a05
vishwakanna <vishwasanthosh2005@gmail.com>
2024-10-11 12:53:04 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,9 +1,17 @@
 let inpbox=document.querySelector("input[type=text]")
 let butt=document.getElementById("button1")
 butt.addEventListener("click",() => {
-        let temp = document.createElement("li")
+    if (inpbox.value=="") {alert("i know you are are a lazy motherfucker but you have to write something")}
+    else{   
+       let flag= document.createElement("input")
+         let temp = document.createElement("li")
+         flag.setAttribute("type","checkbox")
+        flag.setAttribute("id","check")
+        temp.appendChild(flag)
         let txt = document.createTextNode(inpbox.value)
         temp.appendChild(txt)
         document.getElementById("list").appendChild(temp)
+        inpbox.value=""
+    }
 
     })
